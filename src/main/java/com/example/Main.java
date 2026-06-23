c7fd83614918170e81c825216e97f07c91c57899
Jhonnathan Scarcela <jhonnathascarcelaoliv22@gmail.com>
2026-06-22 22:26:06 -0300

    test: Adicionando os testes

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,10 +4,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.List;
 
-import com.example.entities.Department;
-import com.example.entities.Seller;
-
+import com.example.dao.DAOFactory; // Certifique-se de importar a classe DAOFactory corretamente
+import com.example.dao.DepartmentDAO; // Certifique-se de importar a interface DepartmentDAO corretamente
+import com.example.dao.SellerDAO; // Certifique-se de importar a interface SellerDAO corretamente
+import com.example.entities.Department; // Certifique-se de importar a classe Department corretamente
+import com.example.entities.Seller; // Certifique-se de importar a classe Seller corretamente
 public class Main {
     public static void main(String[] args) {
         String url = "jdbc:postgresql://localhost:5432/loja_jdbc";
@@ -25,6 +28,30 @@ public class Main {
         Seller s1 = new Seller(1, "João Silva", "joao.silva@email.com", LocalDate.of(1990, 5, 15), 5000.0, d1);
 
         System.out.println("Departamento: " + s1.getDepartment().getName());
-        System.out.printf("Vendedor: %s, Email: %s%nData de Nascimento: %s, Salário Base: %.2f \n", s1.getName(), s1.getEmail(), s1.getBirthDate(), s1.getBaseSalary());   
+        System.out.printf("Vendedor: %s, Email: %s%nData de Nascimento: %s, Salário Base: %.2f \n", s1.getName(), s1.getEmail(), s1.getBirthDate(), s1.getBaseSalary());
+        
+        DepartmentDAO departmentDAO = DAOFactory.createDepartmentDAO();
+        SellerDAO sellerDAO = DAOFactory.createSellerDAO();
+        // Teste de inserção de departamento
+        // Passamos null para o ID, pois ele será gerado automaticamente pelo banco de
+        // dados
+        Department newDepartment = new Department(null, "Marketing");
+        departmentDAO.insert(newDepartment);
+        // Teste de consulta de departamentos
+        List<Department> departments = departmentDAO.findAll();
+        System.out.println("Departamentos encontrados:");
+        for (Department dept : departments) {
+            System.out.println(dept.getId() + ": " + dept.getName());
+        }
+        // Teste de inserção de vendedor
+        Seller newSeller = new Seller(null, "Maria Oliveira", "maria.oliveira@example.com",
+                LocalDate.parse("1990-05-15"), 5000.0, departments.getLast()); // Associando o vendedor ao último departamento inserido (Marketing)
+        sellerDAO.insert(newSeller);
+        // Teste de consulta de vendedores
+        List<Seller> sellers = sellerDAO.findAll();
+        System.out.println("Vendedores encontrados:");
+        for (Seller seller : sellers) {
+            System.out.println(seller.getId() + ": " + seller.getName());
+        }
     }
 }
